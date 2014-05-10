71960946ee711efa1e20b7851ba3ef949be3e46c
GHowlett <griff.howl@cd>
2014-05-10 16:44:26 -0700

    live tweets finished

--- www/main.js
+++ www/main.js
@@ -1,6 +1,15 @@
-var socket = io.connect('http://localhost');
+var socket = io.connect('http://localhost'); //TODO: change to just be root
 
 socket.on('tweet', function(tweet){
-	// TODO: insert tweets into #tweets list
-	console.log(tweet);
+	var tweetEl = $('<div class="tweet">');
+
+	tweetEl.append('<img class="profile-img" src="' + tweet.user.profile_image_url_https + '">');
+
+	var header = $('<div class="tweet-header">').appendTo(tweetEl);
+	header.append('<span class="username">' + tweet.user.name + '</span>');
+	header.append('<span class="date"> - ' + prettyDate(tweet.created_at) + '</span>');
+
+	tweetEl.append('<p>' + tweet.text + '</p>');
+
+	$('#tweets').prepend(tweetEl);
 });
