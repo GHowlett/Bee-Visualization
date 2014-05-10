fe625250acc43fb0d46762f40add09f2cdd754d7
GHowlett <griff.howl@cd>
2014-05-10 15:01:50 -0700

    real time twitter stream listener

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 var path = require('path');
 var express = require('express');
+var twit = new (require('twit'))({
+	consumer_key: 'MHA5zldY2vrNq4wq6u1ndVfJk',
+	consumer_secret: 'vNWlPX6grC1bphD7iZGC22i1veXqsNfI9Z6xt84onBT7aYOW5Y',
+	access_token: '2488844486-D7JiVnPodmEhMzz34FJuDDSOjjwgcswvmRZTTDX',
+	access_token_secret: 'MkG6696eABas1DLiUWOxxcNAI5XMxFDroPQv5xwf9QL6M'
+});
 
-var app = express()
+//var twitStream = twit.stream('statuses/filter', {track: '#TerminalBee'});
+var twitStream = twit.stream('user');
+
+twitStream.on('tweet', function (tweet) {
+	// TODO: emit update to website
+});
+
+var server = express()
 	.set('views', __dirname + 'www')
 	.use(express.static(__dirname + '/www'))
 	.use(express.static(__dirname + '/bower_components'))
 	.use(express.bodyParser())
 
 var port = process.env.PORT || 3000;
-app.listen(port);
+server.listen(port);
 console.log(__filename + ' is now listening on port ' + port);
 
